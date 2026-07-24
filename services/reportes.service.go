1427d691ef7d33ba08259e381e7befbf2a8b1490
Stiven Osorio <jsosoriov@udistrital.edu.co>
2026-07-24 15:36:56 -0500

    feature: agrega tipo de reporte 0 (todos los inscritos),reportes dinamicos udistrital/sga_documentacion#714
    Nuevo TipoReporte 0 en ReporteDinamico y reporteInscritosPorPrograma que lista todos los inscritos sin filtrar por tipo/estado. Incluye consulta a ConsultarReciboJbpmService para obtener estado y pago del recibo. Headers extendidos en generarXlsxyPdfIncripciones.

--- services/reportes.service.go
+++ services/reportes.service.go
@@ -1581,8 +1581,8 @@ func reporteInscritosPorPrograma(infoReporte models.ReporteEstructura) requestre
 	} else {
 		//Añadir headers no compartidos
 		dataHeader["Indices"] = append(dataHeader["Indices"].([]interface{}),
-			"Tipo inscripción",
-			"Estado inscripción")
+			"Tipo inscripción", "Estado inscripción", "Secuencia", "Fecha de pago",
+		)
 
 		//Hacer consulta sin filtrar por tipo de inscripción (TipoReporte 0)
 		errInscripciones = request.GetJson(beego.AppConfig.String("InscripcionService")+fmt.Sprintf("inscripcion?query=Activo:true,ProgramaAcademicoId:%v,PeriodoId:%v&limit=0", infoReporte.Proyecto, infoReporte.Periodo), &inscripciones)
@@ -1628,8 +1628,22 @@ func reporteInscritosPorPrograma(infoReporte models.ReporteEstructura) requestre
 			} else if infoReporte.TipoReporte == 2 {
 				enfasis := obtenerEnfasis(fmt.Sprintf("%v", inscripcion["EnfasisId"]))
 				inscrito = append(inscrito, inscripcion["Id"], enfasis, inscripcion["EstadoInscripcionId"].(map[string]interface{})["Nombre"], inscripcion["NotaFinal"])
-			} else {
+			} else if infoReporte.TipoReporte == 3 {
 				inscrito = append(inscrito, inscripcion["TipoInscripcionId"].(map[string]interface{})["Nombre"], inscripcion["EstadoInscripcionId"].(map[string]interface{})["Nombre"])
+			} else {
+				// tipo de reporte 0 -> TODOS LOS ASPIRANTES SIN IMPORTAR ESTADO
+				var ReciboResp models.ReciboResponse
+				errRecibo := request.GetJsonWSO2(beego.AppConfig.String("ConsultarReciboJbpmService")+"consulta_recibo/"+fmt.Sprintf("%v", inscripcion["ReciboInscripcion"]), &ReciboResp)
+				logs.Info("ReciboResp para inscripcion %v: errRecibo=%v, ReciboResp=%v", inscripcion["Id"], errRecibo, ReciboResp)
+				reciboEstado := "NA"
+				reciboPago := "NA"
+				if errRecibo == nil && len(ReciboResp.ReciboCollection.Recibo) > 0 {
+					recibo := ReciboResp.ReciboCollection.Recibo[0]
+					reciboEstado = fmt.Sprintf("%v/%v", recibo.Secuencia, recibo.Ano)
+					reciboPago = fmt.Sprintf("%v", recibo.FechaPagado)
+				}
+				logs.Info("ReciboResp resultado para inscripcion %v: estado=%v, pago=%v", inscripcion["Id"], reciboEstado, reciboPago)
+				inscrito = append(inscrito, inscripcion["TipoInscripcionId"].(map[string]interface{})["Nombre"], inscripcion["EstadoInscripcionId"].(map[string]interface{})["Nombre"], reciboEstado, reciboPago)
 			}
 			inscritos = append(inscritos, inscrito)
 
